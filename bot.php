a8c97825e0fa3f3e37c1f9210341c9c22d57ee27
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-07 15:48:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,5 @@
 <?php
-$access_token = '8auxoYNmwKKiI0DqQwDHmVIP+r/Bef6dZbTdnPPNY46xKD51B1sQbcHsEOeIYZr0IkjdLRzyNNfQ+QuADbqU4rK9qErTtQi1QwOy+aQHkuGgkYDWZK32SDWrcfsQhQoKmhgqMrt+SXNOHkKNs/n46QdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'IECWvICftD2m9F5X2UCtIKeWXYn5E6X6zxxlvrS/gP5msX7xnsCxqmF7yEO1BSyetXM3EjMY6gqVi3FObND2OcblW87bC7DD4T0+i5hGUzr1Qio45Pw/JAPie8Ycyvl5/QtBQ22/aq/3DPufaHWb0wdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
